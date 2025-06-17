fe574aa7f09444e580435c179da31b97237d9352
Peter <thepj04@gmail.com>
2025-06-16 21:27:08 -0400

    Added comment

--- SmsRateLimiter/Api/HistoryResources/HistoryHandler.cs
+++ SmsRateLimiter/Api/HistoryResources/HistoryHandler.cs
@@ -6,7 +6,8 @@ public class HistoryHandler
 {
     public static async Task<IResult> SendHistoryRecords(HistoryLog historyLog)
     {
-        // Add implementation here
+        // Just a note; in a real world scenario, the structure returned in an endpoint
+        // should not reflect the structure of the underlying data i.e. the DB schema
         var historyEntries = historyLog.GetEntries();
         return Results.Ok(historyEntries);
     }
